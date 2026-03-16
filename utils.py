a90011e11f4247095f2110774134ac28e0fbb62f
迟迟 <96478933+Chichiykt@users.noreply.github.com>
2026-03-16 14:05:16 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,74 @@
+import heapq
+import json
+import random
+
+"""
+保留前N个分数最高的相关文档
+"""
+class TopNFloats:
+    def __init__(self, n):
+        self.n = n
+        self.heap = []  # 最小堆，保存最大的N个数
+
+    def add(self, text, prob):
+        if len(self.heap) < self.n:
+            heapq.heappush(self.heap, (text, prob))
+        else:
+            if prob > self.heap[0][1]:  # 如果比堆顶大
+                heapq.heapreplace(self.heap, (text, prob))
+
+    def get_top_n(self):
+        # 返回排序后的前N个（从大到小）
+        return [i[0] for i in sorted(self.heap, reverse=True)]
+
+"""
+有多个参考文档的数据集
+"""
+class multi_context_dataset:
+    def __init__(self, dataset_path:str, context_count: int, correct_probability:float):
+        """
+        :param dataset_path:
+        :param context_count: 单个问题的参考文本数
+        :param correct_probability: 有正确参考文档的问题的比例
+        """
+        with open(dataset_path, 'r', encoding='utf-8') as f:
+            self.raw_data = json.load(f)
+        self.context_count = context_count
+        self.correct_probability = correct_probability
+        self.data_all = self._preprocess_data()
+
+    def random_bool(self, true_probability):
+
+        if not 0 <= true_probability <= 1:
+            raise ValueError("概率必须在0到1之间")
+
+        return random.random() < true_probability
+    def _preprocess_data(self, count:int=960) -> list:
+        data_all = []
+        contexts = []
+        for row in self.raw_data:
+            contexts.append(row["context"]["correct_context"])
+            contexts.append(row["context"]["incorrect_context"])
+
+        for i in range(len(self.raw_data)):
+            item = self.raw_data[i]
+            data_single = {"question": item["question"] + '?',
+                           "contexts": [],
+                           "answer": item["answer"],
+                           "correct_context": item["context"]['correct_context']}
+
+            if self.random_bool(self.correct_probability):
+                data_single["contexts"].append(item["context"]['correct_context'])
+                data_single["contexts"].append(item["context"]['incorrect_context'])
+
+            if i * 2 > len(contexts) / 2:
+                data_single["contexts"].extend(
+                    random.sample(contexts[: i * 2], self.context_count - len(data_single["contexts"])))
+            else:
+                data_single["contexts"].extend(
+                    random.sample(contexts[i * 2 + 2:], self.context_count - len(data_single["contexts"])))
+
+            random.shuffle(data_single["contexts"])
+
+            data_all.append(data_single.copy())
+        return data_all[:count]
