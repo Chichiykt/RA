b4d12840fe1d6d7702ec40ce3635751bef49a944
迟迟 <96478933+Chichiykt@users.noreply.github.com>
2026-03-16 14:49:47 +0800

    Update nli_model.py

--- model/nli_model.py
+++ model/nli_model.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 class NLIModel:
-    """自然语言推理模型，判断答案是否正确"""
+    """Natural language reasoning model, determining whether the answer is correct"""
     def __init__(self, model_path: str, gpu_idx: int):
         self.device = torch.device(f"cuda:{gpu_idx}" if torch.cuda.is_available() else "cpu")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -25,7 +25,7 @@ class NLIModel:
             predictions = torch.softmax(outputs.logits, dim=-1)
             predicted_class = torch.argmax(predictions, dim=-1).item()
             print("premise: ", premise, "\nhypothesis: ", hypothesis , "\npredictions: ", predictions, "\npredicted_class: ", predicted_class)
-            # 根据模型输出映射到标签
+            # Map the model output to the label
             label_map = {0: "entailment", 1: "neutral", 2: "contradiction"}
             return label_map[predicted_class], predictions[0][predicted_class].cpu().numpy().item()
 
